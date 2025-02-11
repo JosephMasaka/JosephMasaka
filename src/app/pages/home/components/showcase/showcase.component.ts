6350965a3ee14bf523a007a3a5182553b57ac429
JosephMasaka <masakajoseph14@gmail.com>
2025-02-11 16:16:58 +0300

    showcase: homepage cosmetic changes, home component added about and technical acumen

--- src/app/pages/home/components/showcase/showcase.component.ts
+++ src/app/pages/home/components/showcase/showcase.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-showcase',
   standalone: true,
-  imports: [],
   templateUrl: './showcase.component.html',
   styleUrl: './showcase.component.css'
 })
-export class ShowcaseComponent {
+export class ShowcaseComponent implements AfterViewInit {
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngAfterViewInit(): void {
+    // Ensure this code only runs in the browser
+    if (isPlatformBrowser(this.platformId)) {
+      const scrollButton = document.querySelector('.scroll-down');
+
+      if (scrollButton) {
+        scrollButton.addEventListener('click', (event) => {
+          event.preventDefault();
+          document.querySelector('#next-section')?.scrollIntoView({ behavior: 'smooth' });
+        });
+      }
+    }
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
 import { ShowcaseComponent } from './components/showcase/showcase.component';
+import { AboutBriefComponent } from './components/about-brief/about-brief.component';
+import { TechnicalAcumenComponent } from './components/technical-acumen/technical-acumen.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, ShowcaseComponent],
+  imports: [CommonModule, NavbarComponent, ShowcaseComponent, AboutBriefComponent, TechnicalAcumenComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
