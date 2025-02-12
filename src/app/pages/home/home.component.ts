1bbad0ce90c2c6f9330cb3fe16cd75f325a0c040
JosephMasaka <masakajoseph14@gmail.com>
2025-02-12 09:31:52 +0300

    home component simple mods

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,11 +4,12 @@ import { NavbarComponent } from '../../shared/navbar/navbar.component';
 import { ShowcaseComponent } from './components/showcase/showcase.component';
 import { AboutBriefComponent } from './components/about-brief/about-brief.component';
 import { TechnicalAcumenComponent } from './components/technical-acumen/technical-acumen.component';
+import { ValuePrepositionComponent } from './components/value-preposition/value-preposition.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, ShowcaseComponent, AboutBriefComponent, TechnicalAcumenComponent],
+  imports: [CommonModule, NavbarComponent, ShowcaseComponent, AboutBriefComponent, TechnicalAcumenComponent, ValuePrepositionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
